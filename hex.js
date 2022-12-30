75ed7212b9dc366495211ed9de4ffebd534799f3
Luís Gabriel Viana Barrichello <luisgbarrichelo@gmail.com>
2022-12-30 12:50:12 -0300

    corrigindo hex.js

--- hex.js
+++ hex.js
@@ -1,5 +1,5 @@
 //Array para gerar colres de fundo
-const hex = [ 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , " A " , " B " , " C " , " D " , " E " , " F " ];
+const hex = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A" , "B" , "C" , "D" , "E" , "F" ];
 
 //pegando botao pelo seu ID
 const button = document.getElementById('btn');
